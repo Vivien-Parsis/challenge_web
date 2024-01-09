dc079ab42663ef7f4d10ecd32621656fd1dff351
VIvien-Parsis <vivienparsis.perso@gmail.com>
2024-01-09 09:09:50 +0100

    added button scroll to top

---
+++ assets/js/scrollToTop.js
@@ -0,0 +1,13 @@
+const scrollToTop = () => {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
+const scrollVisibility = () => {
+    if(document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) 
+    {document.getElementById("BackToTop").style.display = "block"; } 
+    else 
+    {document.getElementById("BackToTop").style.display = "none";}
+}
+
+document.getElementById("BackToTop").onclick = scrollToTop;
+window.onscroll = () => {scrollVisibility()};
